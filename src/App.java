091b5a80d9e75133fee45082031f6986310fc1cc
Your NameTiagoCos <tiagocostasilva27@gmail.com>
2023-11-17 22:27:16 -0300

    .

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
       int nota = 40;
        String resultado = nota >= 70 ? "aprovado" : "reprovado";
         System.out.println("o aluno foi " + resultado);
         
-
+        
     }
 }
